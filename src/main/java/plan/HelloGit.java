fdc59bf057168eefc52ac804ab7d87d34193ffa6
随风枫叶 <1527261080@qq.com>
2016-03-06 11:17:38 +0800

    已标识解决了冲突

--- src/main/java/plan/HelloGit.java
+++ src/main/java/plan/HelloGit.java
@@ -3,5 +3,10 @@ package plan;
 public class HelloGit {
 	public void okayToSay(){
 		System.out.println("hello, gay");
+		/*----------------*/
+		System.out.println("nice to here!!!");
+	}
+	public void fuckyou(){
+		System.out.println("may be you modify too!!! ");
 	}
 }
