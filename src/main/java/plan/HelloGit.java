4f5cd4af0f255ce43a4441a64681cf1674e5b263
随风枫叶 <1527261080@qq.com>
2016-03-06 13:55:04 +0800

    dev ffff

--- src/main/java/plan/HelloGit.java
+++ src/main/java/plan/HelloGit.java
@@ -12,4 +12,7 @@ public class HelloGit {
 	public void fuckyou2(){
 		System.out.println("may be you modify too222!!!gai yi ge bug");
 	}
+	public void fuckyou3(){
+		System.out.println("may be you modify too3333!!!");
+	}
 }
